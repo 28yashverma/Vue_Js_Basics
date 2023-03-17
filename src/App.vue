8ab2df8577f9e3863d6a21175b7685c97e41d2ce
28yashverma <28.yashverma@gmail.com>
2023-03-17 16:17:36 +0530

    Computed properties

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,5 @@
 <template>
-  <div>{{greet}} {{name}}</div>
-  <div v-html="channel"> </div>
-  <h2 v-bind:id="headingId">Heading</h2>
-  <button v-bind:disabled="isDisabled">Button</button>
-  <h2 class="underline">Underline text</h2>
-  <h2 v-bind:class="status">Status</h2>
-  <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
-  <h2 v-bind:class="isSoldOut ? 'issoldOut' : 'new'">SoldOut? Movie</h2>
-  <h2 v-bind:class="['new', 'promoted']">Newly promoted movie</h2>
-  <h2 v-bind:class="{
-    promoted: isPromoted,
-    new: !isSoldOut,
-    isSoldOut: isSoldOut
-  }">Object conditional movie</h2>
-
-  <h2 v-bind:style="{
-    color: highlightColor,
-    'font-size': headerSize + 'px'
-  }">Inline style</h2>
-  <h2 v-bind:style="headerStyleObject">Style Object</h2>
-
-  <h2 v-if="num === 0">The number is zero</h2>
-  <h2 v-else-if="num < 0">the number is negative</h2>
-  <h2 v-else-if="num > 0">the number is positive</h2>
-  <h2 v-else>Not a number</h2>
-
-  <div v-if="display">
-    <h2>Yeshendra</h2>
-    <h2>My code</h2>
-    <h2>Vue</h2>
-  </div>
-
-  <h2 v-show="display">Using v-show</h2>
-
-  <h2 v-for="(n, index) in names" :key="n">{{index}} {{ n }}</h2>
-  <h2 v-for="name in fullNames" :key="name.first">{{ name.first }} {{ name.last }}</h2>
-
-  <template v-for="name in names" :key="name">
-    <h2 v-if="name === 'Bruce'">{{ name }}</h2>
-  </template>
-  <!--Calling a method-->
-  <h2>Add method - {{ add(2, 4, 6) }}</h2>
-  <h2>Multiply method - {{ multiply(4, $event) }}</h2>
-
-  <!--Event handling -->
-  <div>
-    <button v-on:click="name='Batman'">Change name</button>
-  </div>
-  <h2>{{ count }}</h2>
-  <div>
-    <button @mouseover="increment()">Increment</button>
-    <button @mouseover="decrement()">Decrement</button>
-  </div>
+  <h2>Computed Full name : {{ fullName }}</h2>
 </template>
 
 <script>
@@ -61,44 +9,16 @@ export default {
   name: 'App',
   data() {
     return {
-      greet: "Hello",
-      name: "Yeshendra",
-      channel: "<b>My channel</b>",
-      headingId: 'heading',
-      isDisabled: false, 
-      status: 'danger',
-      isPromoted: true,
-      isSoldOut:false,
-      highlightColor: 'orange',
-      headerSize: 25,
-      headerStyleObject: {
-        color: 'orange',
-        fontSize: 50 + 'px'
-      },
-      num: 5, 
-      display: true, 
-      names: ['Bruce', 'Clarke', 'Diana'], 
-      fullNames: [
-        {first: 'Bruce', last: 'Wayne'},
-        {first: 'Clarke', last: 'Kent'}
-      ],
-      baseMultiplier: 10,
-      count: 0
+      firstName: "Bruce",
+      lastName: "Wayne"
     };
   },
   methods: {
-    add(a, b, c) {
-      return a+b+c;
-    },
-    multiply(num, event) {
-      console.log(event)
-      return this.baseMultiplier * num;
-    },
-    increment() {
-      return this.count++;
-    },
-    decrement() {
-      return this.count--;
+    
+  },
+  computed: {
+    fullName(){
+      return `${this.firstName} ${this.lastName}`
     }
   },
   components: {
@@ -117,19 +37,5 @@ export default {
   margin-top: 60px;
 }
 
-.underline {
-  text-decoration: underline;
-}
 
-.promoted {
-  font-style: italic;
-}
-
-.new {
-  color: green;
-}
-
-.issoldOut {
-  color: red;
-}
 </style>
